1d0bb5cd6a6865c548da7ad9287f2cf34ad8ec88
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-06-09 22:13:26 +0530

    [Aryan] Add: Assignment Question Earth volume

---
+++ gcr-codebase/programming-elements/Level1/EarthVolume.java
@@ -0,0 +1,19 @@
+package level1;
+
+import java.util.Scanner;
+
+public class EarthVolume {
+    public static void main(String[] args) {
+
+        double radiusKm = 6378;
+
+        double volumeKm = (4.0 / 3.0) * Math.PI * Math.pow(radiusKm, 3);
+
+        double radiusMiles = radiusKm * 0.621371;
+
+        double volumeMiles = (4.0 / 3.0) * Math.PI * Math.pow(radiusMiles, 3);
+
+        System.out.println("The volume of Earth in cubic kilometers is " + volumeKm);
+        System.out.println("The volume of Earth in cubic miles is " + volumeMiles);
+    }
+}
