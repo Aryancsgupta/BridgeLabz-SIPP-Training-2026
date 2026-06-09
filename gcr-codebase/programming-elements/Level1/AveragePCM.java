1f5a0b64e8400c4de1181f5659778534d23120a4
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-06-09 22:12:47 +0530

    [Aryan] Add: Assignment Question Average PCM

---
+++ gcr-codebase/programming-elements/Level1/AveragePCM.java
@@ -0,0 +1,22 @@
+package level1;
+
+import java.util.Scanner;
+
+public class AveragePCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Maths Marks: ");
+        int maths = sc.nextInt();
+
+        System.out.print("Enter Physics Marks: ");
+        int physics = sc.nextInt();
+
+        System.out.print("Enter Chemistry Marks: ");
+        int chemistry = sc.nextInt();
+
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam's average mark in PCM is " + average);
+    }
+}
