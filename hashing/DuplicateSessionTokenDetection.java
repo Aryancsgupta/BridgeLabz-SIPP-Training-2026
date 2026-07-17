3ded8003c986c6fe7d2ff2fe550c4abd63aa3df2
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-07-17 22:06:39 +0530

    [Aryan] add : Hashing assignment

---
+++ hashing/DuplicateSessionTokenDetection.java
@@ -0,0 +1,10 @@
+public boolean hasDuplicateToken(String[] tokens) {
+    Set<String> seen = new HashSet<>();
+
+    for (String token : tokens) {
+        if (!seen.add(token))
+            return true;
+    }
+
+    return false;
+}
